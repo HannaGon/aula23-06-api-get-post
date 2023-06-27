2936f077803e8f6c61ecf29e126092a47f111fc4
Bruno Hirata <bruno.n.hirata@outlook.com>
2023-06-27 19:30:38 -0300

    Deletar e atualizar

--- exercicio-02/src/controllers/produtoController.js
+++ exercicio-02/src/controllers/produtoController.js
@@ -1,7 +1,18 @@
 const produtos = require("../models/produtoModel")
+const crypto = require("crypto")
 
 const obterTodos = (request, response) => {
     response.status(200).send(produtos)
+}
+
+const obterPorId = (request, response) => {
+    const id = request.params.id
+    const produto = produtos.find(produto => produto.id == id)
+    if (produto) {
+        response.status(200).send(produto)
+    } else {
+        response.status(404).send({ mensagem: `Não foi encontrado o produto de ID ${id}.` })
+    }
 }
 
 const obterPorNome = (request, response) => {
@@ -14,15 +25,44 @@ const obterPorNome = (request, response) => {
     }
 }
 
+const atualizar = (request, response) => {
+    const id = request.params.id
+    const produtoModel = produtos.find(produto => produto.id == id)
+    if (produtoModel) {
+        Object.keys(produtoModel).forEach(key => {
+            if (request.body.hasOwnProperty(key)) {
+                produtoModel[key] = request.body[key]
+            }
+        })
+        response.status(200).send(produtoModel)
+    } else {
+        response.status(404).send({ mensagem: `Não foi encontrado o produto de ID ${id}.` })
+    }
+}
+
 const cadastrar = (request, response) => {
     const { produto, tipo, preco } = request.body
-    const produtoModel = { produto, tipo, preco }
+    const produtoModel = { id: crypto.randomUUID(), produto, tipo, preco }
     produtos.push(produtoModel)
     response.status(200).send(produtoModel)
 }
 
+const deletar = (request, response) => {
+    const id = request.params.id
+    const produtoIndex = produtos.findIndex(produto => produto.id == id)
+    if (produtoIndex >= 0) {
+        produtos.splice(produtoIndex, 1)
+        response.status(200).send({ message: `O produto de ID ${id} foi deletado.` })
+    } else {
+        response.status(404).send({ mensagem: `Não foi encontrado o produto de ID ${id}.` })
+    }
+}
+
 module.exports = {
     obterTodos,
+    obterPorId,
     obterPorNome,
-    cadastrar
+    atualizar,
+    cadastrar,
+    deletar
 }
--- exercicio-02/src/routes/produtoRoute.js
+++ exercicio-02/src/routes/produtoRoute.js
@@ -4,7 +4,11 @@ const controller = require("../controllers/produtoController")
 const app = express.Router()
 
 app.get("/produto", controller.obterTodos)
+app.get("/produto/:id", controller.obterPorId)
 app.get("/produto/nome/:nome", controller.obterPorNome)
 app.post("/produto", controller.cadastrar)
+app.delete("/produto/:id", controller.deletar)
+app.patch("/produto/:id", controller.atualizar)
+app.put("/produto/:id", controller.atualizar)
 
 module.exports = app
